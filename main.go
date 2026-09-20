109fd1ff6f9efe1f4f3c1bf2c1a16f3a4f1a8096
Ahmad Iman bin Md Nasirudin <imannasirudin@heitech.com.my>
2026-09-20 12:09:25 +0800

    create a simple web server

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+// Define home handler function which write a byte slice "Hello ..." as the response body
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippetbox"))
+}
 
 func main() {
-	fmt.Println("Hello World")
+	
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	log.Print("Starting a server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
